4c628f421df1e249c8da464388f69e1995ab8baa
Abdulsamet Karakaya <56205106+developerkarakaya@users.noreply.github.com>
2021-10-15 16:47:09 +0300

    a

--- Eticaret2022/Utils/BaseController.cs
+++ Eticaret2022/Utils/BaseController.cs
@@ -6,9 +6,13 @@ using System.Web.Mvc;
 
 namespace Eticaret2022.Utils
 {
+
+    /* Samet Mirza Karakaya -- Ramazan İşçanan 
+       Başlangıç Tarihi : 14 Ekim 2021 Perşembe
+       Bitiş Tarihi : ?
+    */
     public class BaseController:System.Web.Mvc.Controller
     {
-
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             // sayfalar arası geçişi session'a göre yöneteceğim.
