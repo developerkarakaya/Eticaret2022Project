54f5fe9366aa415ef796b1818350ed81959d4bf1
Abdulsamet Karakaya <56205106+developerkarakaya@users.noreply.github.com>
2021-10-15 16:41:31 +0300

    d

---
+++ Eticaret2022/Areas/yonetim/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eticaret2022.Areas.yonetim.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: yonetim/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Eticaret2022/Areas/yonetim/yonetimAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Eticaret2022.Areas.yonetim
+{
+    public class yonetimAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "yonetim";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "yonetim_default",
+                "yonetim/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
