ec34b2c5b3efada971a734a9dad800abed8aa10c
Abdulsamet Karakaya <56205106+developerkarakaya@users.noreply.github.com>
2021-10-15 12:03:36 +0300

    Sepet İşlemleri

---
+++ Eticaret2022.BussinessLayer/Basket/Basket.cs
@@ -0,0 +1,59 @@
+﻿using Eticaret2022.DataEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eticaret2022.BussinessLayer.Basket
+{
+    public class Basket 
+    {
+        private List<BasketLine> _basketLines = new List<BasketLine>();
+
+        public List<BasketLine> BasketLines 
+        {
+            get
+            {
+                return _basketLines;
+            }
+        }
+
+
+        public void AddProduct(Urunler urun , int adet)
+        {
+            var line = _basketLines.FirstOrDefault(ss => ss.Urun.Id == urun.Id);
+            if(line ==null)
+            {
+                _basketLines.Add(new BasketLine {
+                Urun =urun,
+                Adet=adet
+                });
+            }
+            else
+            {
+                line.Adet += adet;
+            }
+        }
+
+        public void DeleteProduct(Urunler urun)
+        {
+            _basketLines.RemoveAll(i=>i.Urun.Id==urun.Id);
+        }
+
+        public double TotalPrice()
+        {
+            return (double)_basketLines.Sum(i => i.Urun.UrunFiyat * i.Adet);
+        }
+        public void ClearBasket()
+        {
+            _basketLines.Clear();
+        }
+    }
+
+    public class BasketLine
+    {
+        public Urunler Urun { get; set; }
+        public int Adet { get; set; }
+    }
+}
---
+++ Eticaret2022/Controllers/BasketController.cs
@@ -0,0 +1,31 @@
+﻿using Eticaret2022.BussinessLayer.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eticaret2022.Controllers
+{
+    public class BasketController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public Basket GetBasket()
+        {
+            // sepete ürün ekleme ile ilgili işlem yapılacağı zaman getbasket methodu
+            // çağrılır ve sessionda basket objesi yok ise olusturur. 
+            // uı daha sonra yapılacak şimdilik backend tamamlandı.
+            var Basket = (Basket)Session["Basket"];
+            if(Basket == null)
+            {
+                Basket = new Basket();
+                Session["Basket"] = Basket;
+            }
+            return Basket;
+        }
+    }
+}
