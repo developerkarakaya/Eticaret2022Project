7f1481d7af1596fee733eda5c7d6d8de6c33af61
Abdulsamet Karakaya <56205106+developerkarakaya@users.noreply.github.com>
2021-10-15 12:20:33 +0300

    basker vs service

---
+++ Eticaret.Service/Services/BasketService.cs
@@ -0,0 +1,14 @@
+﻿using Eticaret2022.BussinessLayer.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eticaret.Service.Services
+{
+   public class BasketService
+    {
+       
+    }
+}
--- Eticaret2022/Controllers/BasketController.cs
+++ Eticaret2022/Controllers/BasketController.cs
@@ -20,7 +20,7 @@ namespace Eticaret2022.Controllers
             // çağrılır ve sessionda basket objesi yok ise olusturur. 
             // uı daha sonra yapılacak şimdilik backend tamamlandı.
             var Basket = (Basket)Session["Basket"];
-            if(Basket == null)
+            if (Basket == null)
             {
                 Basket = new Basket();
                 Session["Basket"] = Basket;
